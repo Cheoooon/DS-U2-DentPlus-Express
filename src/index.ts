004617226c4b5e9cd6965646f45960479eddc17e
Ángel David Neida <angel.david.n.n@gmail.com>
2026-05-06 21:37:08 -0400

    Se mueve de lugar las vistas (ya que no deberían estar en src)

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(process.cwd(), 'src', 'views'));
+app.set('views', path.join(process.cwd(), 'views'));
 
 // Middleware
 app.use(express.json());
